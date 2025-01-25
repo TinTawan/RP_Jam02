1eaf540436f32cd8a13b9613e407d3c7cf8d66db
JustW <jw4566c@gre.ac.uk>
2025-01-25 14:07:06 +0000

    teeth lerp chomp now
    so far just bite in - next need to make bite change depending on which button you press

--- GGJ2025/Assets/Scripts/BubbleManChomp.cs
+++ GGJ2025/Assets/Scripts/BubbleManChomp.cs
@@ -9,6 +9,8 @@ public class BubbleManChomp : MonoBehaviour
 
     Vector2 topOrigin, bottomOrigin;
 
+    [SerializeField] float chompTime = 0.25f;
+
     bool biting;
 
     private void Start()
@@ -22,30 +24,42 @@ public class BubbleManChomp : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.W))
         {
             biting = true;
         }
 
         if (biting)
         {
-            StartCoroutine(Bite());
+            ChompDown();
         }
+    }
+
+    void ChompDown()
+    {
+        topTeeth.transform.position = Vector2.Lerp(topTeeth.transform.position, topMoveTo.position, /*Time.deltaTime * */chompTime);
+        bottomTeeth.transform.position = Vector2.Lerp(bottomTeeth.transform.position, bottomMoveTo.position, /*Time.deltaTime * */chompTime);
+
+        StartCoroutine(Bite());
+
     }
 
     IEnumerator Bite()
     {
-        topTeeth.transform.position = topMoveTo.position;
-        bottomTeeth.transform.position = bottomMoveTo.position;
+        //topTeeth.transform.position = topMoveTo.position;
+        //bottomTeeth.transform.position = bottomMoveTo.position;
+
+        //topTeeth.transform.position = Vector2.Lerp(topOrigin, topMoveTo.position, Time.deltaTime * chompTime);
+        //bottomTeeth.transform.position = Vector2.Lerp(bottomOrigin, bottomMoveTo.position, Time.deltaTime * chompTime);
+
+
+
+        yield return new WaitForSeconds(chompTime);
 
         biting = false;
 
-        yield return new WaitForSeconds(0.2f);
-
         topTeeth.transform.position = topOrigin;
         bottomTeeth.transform.position = bottomOrigin;
-
-
 
     }
 }
