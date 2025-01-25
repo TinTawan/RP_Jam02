579f8988ca698b4b1524810a9ae0116fe683ac35
oswal99 <118828910+oswal99@users.noreply.github.com>
2025-01-25 09:06:06 +0000

    One-Button
    A button were implement in the game to create the branch.
    Pd: Working on the Ui of the game.

---
+++ GGJ2025/Assets/Scripts/Os/Button.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Os");
+        Debug.Log("Wuw");
+    }
+
+}
